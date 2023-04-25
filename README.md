192e0e35a57e8695ecc427de2f6cfa59f50d4bc8
Ckk3 <contato.gustavomota@gmail.com>
2023-04-24 21:12:05 -0300

    add new routes

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This is a repo containing the files I created while learning FastAPI.
 course link: https://www.youtube.com/watch?v=GN6ICac3OXY
 Thanks AmigosCode!
+
+
+## How run:
+install dependencies in `requirements.txt`, then run this command to start the server  
+
+```bash
+uvicorn main:app --reload
+```
--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+import subprocess
+import os
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+
+@app.get("/name")
+def name(name: str):
+    return {"name": name}
+
+
+@app.get("/pm2logs")
+def read_pm2logs():
+    proc = subprocess.run(
+        ["pm2", "logs", "--lines", "100", "--nostream"], capture_output=True
+    )
+    output = proc.stdout
+    return {"output": output}
+
+
+@app.delete("/deletefolder")
+def delete_folder():
+    try:
+        os.rmdir("testfolder")
+        return {"output": "File deleted successfully"}
+    except OSError as error:
+        raise Exception(f"File deletion failed: {error}")
